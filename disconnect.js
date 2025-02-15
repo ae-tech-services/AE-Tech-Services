7e69d8563db566b61c3332dc79e574ad215ed634
AE Services <100210710+AEservices@users.noreply.github.com>
2025-02-15 11:34:44 +0800

    Update disconnect.js

--- disconnect.js
+++ disconnect.js
@@ -1,3 +1,9 @@
+// Prevent future dates in Payment Date field
+    document.addEventListener("DOMContentLoaded", function() {
+        let today = new Date().toISOString().split("T")[0]; // Get today's date in YYYY-MM-DD format
+        document.getElementById("paidDate").setAttribute("max", today);
+    });
+
 function openInDefaultBrowser() {
     var url = "https://ae-tech-services-receipt-bz3bf.kinsta.page/";
 
