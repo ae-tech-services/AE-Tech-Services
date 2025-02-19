4916a4ee7e4fe12efa43830271933aa0952fd2be
AE Tech Services <100210710+ae-tech-services@users.noreply.github.com>
2025-02-19 15:55:38 +0800

    Update disconnect.js

--- disconnect.js
+++ disconnect.js
@@ -5,7 +5,7 @@
     });
 
 function openInDefaultBrowser() {
-    var url = "https://ae-tech-services.github.io/AE-Tech-Services/sned.html";
+    var url = "https://ae-tech-services.github.io/AE-Tech-Services/send.html";
 
     // ✅ For Android: Open in default browser
     var androidIntent = "intent://" + url.replace("https://", "").replace("http://", "") + "#Intent;action=android.intent.action.VIEW;scheme=https;end;";
